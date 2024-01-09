d639813212cb4df90b504ccaeb55252c845b0cb8
smallflowercat1995 <144557489+smallflowercat1995@users.noreply.github.com>
2024-01-09 16:08:59 +0800

    Update set-sing-box.sh
    重新调整风格

--- set-sing-box.sh
+++ set-sing-box.sh
@@ -1,154 +1,153 @@
 #!/usr/bin/env bash
 # 前戏处理
 initall(){
-   # 同步时间
-   date '+%Y-%m-%d %H:%M:%S'
-   sudo ln -sfv /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-   sudo cat << EOL | sudo tee /etc/timezone
-   Asia/Shanghai
-   EOL
-   date '+%Y-%m-%d %H:%M:%S'
-   
-   # 安装必备工具
-   sudo apt update ; sudo apt-get install -y aria2 catimg git locales curl wget tar socat qrencode uuid net-tools jq
-   
-   # clone udp tcp 互转工具
-   #sudo git clone https://github.com/mullvad/udp-over-tcp.git ; cd udp-over-tcp
-   #sudo bash build-static-bins.sh
-   #sudo mv -fv $(find . -iname "tcp2udp") /usr/bin/
-   #sudo mv -fv $(find . -iname "udp2tcp") /usr/bin/
-   #cd -
-   #sudo rm -rfv udp-over-tcp
-   sudo wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts --continue --retry-connrefused --waitretry=1 --timeout=30 --tries=3 "https://github.com/smallflowercat1995/Sing-Box-Ubuntu-Actions-Workflow/raw/master/tcp2udp" -O /usr/bin/tcp2udp ; sudo chmod -v +x /usr/bin/tcp2udp
-   sudo wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts --continue --retry-connrefused --waitretry=1 --timeout=30 --tries=3 "https://github.com/smallflowercat1995/Sing-Box-Ubuntu-Actions-Workflow/raw/master/udp2tcp" -O /usr/bin/udp2tcp ; sudo chmod -v +x /usr/bin/udp2tcp
-   
-   # Configuration for locales
-   sudo perl -pi -e 's/# zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/g' /etc/locale.gen
-   sudo perl -pi -e 's/en_GB.UTF-8 UTF-8/# en_GB.UTF-8 UTF-8/g' /etc/locale.gen
-   sudo locale-gen zh_CN ; sudo locale-gen zh_CN.UTF-8
-   
-   cat << EOF | sudo tee /etc/default/locale
-   LANGUAGE=zh_CN.UTF-8
-   LC_ALL=zh_CN.UTF-8
-   LANG=zh_CN.UTF-8
-   LC_CTYPE=zh_CN.UTF-8
-   EOF
-   
-   cat << EOF | sudo tee -a /etc/environment
-   export LANGUAGE=zh_CN.UTF-8
-   export LC_ALL=zh_CN.UTF-8
-   export LANG=zh_CN.UTF-8
-   export LC_CTYPE=zh_CN.UTF-8
-   EOF
-   
-   cat << EOF | sudo tee -a $HOME/.bashrc
-   export LANGUAGE=zh_CN.UTF-8
-   export LC_ALL=zh_CN.UTF-8
-   export LANG=zh_CN.UTF-8
-   export LC_CTYPE=zh_CN.UTF-8
-   EOF
-   
-   cat << EOF >> $HOME/.profile
-   export LANGUAGE=zh_CN.UTF-8
-   export LC_ALL=zh_CN.UTF-8
-   export LANG=zh_CN.UTF-8
-   export LC_CTYPE=zh_CN.UTF-8
-   EOF
-   
-   sudo update-locale LANG=zh_CN.UTF-8 LC_ALL=zh_CN.UTF-8 LANGUAGE=zh_CN.UTF-8 LC_CTYPE=zh_CN.UTF-8
-   
-   locale ; locale -a ; cat /etc/default/locale
-   
-   source /etc/environment $HOME/.bashrc $HOME/.profile
+    # 同步时间
+    date '+%Y-%m-%d %H:%M:%S'
+    sudo ln -sfv /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+    sudo cat << EOL | sudo tee /etc/timezone
+    Asia/Shanghai
+    EOL
+    date '+%Y-%m-%d %H:%M:%S'
+
+    # 安装必备工具
+    sudo apt update ; sudo apt-get install -y aria2 catimg git locales curl wget tar socat qrencode uuid net-tools jq
+
+    # clone udp tcp 互转工具
+    #sudo git clone https://github.com/mullvad/udp-over-tcp.git ; cd udp-over-tcp
+    #sudo bash build-static-bins.sh
+    #sudo mv -fv $(find . -iname "tcp2udp") /usr/bin/
+    #sudo mv -fv $(find . -iname "udp2tcp") /usr/bin/
+    #cd -
+    #sudo rm -rfv udp-over-tcp
+    sudo wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts --continue --retry-connrefused --waitretry=1 --timeout=30 --tries=3 "https://github.com/smallflowercat1995/Sing-Box-Ubuntu-Actions-Workflow/raw/master/tcp2udp" -O /usr/bin/tcp2udp ; sudo chmod -v +x /usr/bin/tcp2udp
+    sudo wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts --continue --retry-connrefused --waitretry=1 --timeout=30 --tries=3 "https://github.com/smallflowercat1995/Sing-Box-Ubuntu-Actions-Workflow/raw/master/udp2tcp" -O /usr/bin/udp2tcp ; sudo chmod -v +x /usr/bin/udp2tcp
+
+    # Configuration for locales
+    sudo perl -pi -e 's/# zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/g' /etc/locale.gen
+    sudo perl -pi -e 's/en_GB.UTF-8 UTF-8/# en_GB.UTF-8 UTF-8/g' /etc/locale.gen
+    sudo locale-gen zh_CN ; sudo locale-gen zh_CN.UTF-8
+
+    cat << EOF | sudo tee /etc/default/locale
+    LANGUAGE=zh_CN.UTF-8
+    LC_ALL=zh_CN.UTF-8
+    LANG=zh_CN.UTF-8
+    LC_CTYPE=zh_CN.UTF-8
+    EOF
+
+    cat << EOF | sudo tee -a /etc/environment
+    export LANGUAGE=zh_CN.UTF-8
+    export LC_ALL=zh_CN.UTF-8
+    export LANG=zh_CN.UTF-8
+    export LC_CTYPE=zh_CN.UTF-8
+    EOF
+
+    cat << EOF | sudo tee -a $HOME/.bashrc
+    export LANGUAGE=zh_CN.UTF-8
+    export LC_ALL=zh_CN.UTF-8
+    export LANG=zh_CN.UTF-8
+    export LC_CTYPE=zh_CN.UTF-8
+    EOF
+
+    cat << EOF >> $HOME/.profile
+    export LANGUAGE=zh_CN.UTF-8
+    export LC_ALL=zh_CN.UTF-8
+    export LANG=zh_CN.UTF-8
+    export LC_CTYPE=zh_CN.UTF-8
+    EOF
+
+    sudo update-locale LANG=zh_CN.UTF-8 LC_ALL=zh_CN.UTF-8 LANGUAGE=zh_CN.UTF-8 LC_CTYPE=zh_CN.UTF-8
+
+    locale ; locale -a ; cat /etc/default/locale
+
+    source /etc/environment $HOME/.bashrc $HOME/.profile
 }
 
 # 给变量赋值一个随机的非占用端口
 get_random_port(){
-   #指定端口范围
-   min=$1
-   max=$2
-   #生成一个随机端口
-   port=$(sudo shuf -i $min-$max -n1)
-   #检查端口是否被占用
-   tcp=$(sudo netstat -an | grep ":$port " | awk '$1 == "tcp" && $NF == "LISTEN" {print $0}' | wc -l)
-   udp=$(sudo netstat -an | grep ":$port " | awk '$1 == "udp" && $NF == "0.0.0.0:*" {print $0}' | wc -l)
-   #如果端口被占用，重复上述步骤，直到找到一个空闲的端口
-   while [ $((tcp + udp)) -gt 0 ]; do
-      port=$(sudo shuf -i $min-$max -n1)
-      tcp=$(sudo netstat -an | grep ":$port " | awk '$1 == "tcp" && $NF == "LISTEN" {print $0}' | wc -l)
-      udp=$(sudo netstat -an | grep ":$port " | awk '$1 == "udp" && $NF == "0.0.0.0:*" {print $0}' | wc -l)
-   done
-   #返回端口号
-   echo $port
+    #指定端口范围
+    min=$1
+    max=$2
+    #生成一个随机端口
+    port=$(sudo shuf -i $min-$max -n1)
+    #检查端口是否被占用
+    tcp=$(sudo netstat -an | grep ":$port " | awk '$1 == "tcp" && $NF == "LISTEN" {print $0}' | wc -l)
+    udp=$(sudo netstat -an | grep ":$port " | awk '$1 == "udp" && $NF == "0.0.0.0:*" {print $0}' | wc -l)
+    #如果端口被占用，重复上述步骤，直到找到一个空闲的端口
+    while [ $((tcp + udp)) -gt 0 ]; do
+        port=$(sudo shuf -i $min-$max -n1)
+        tcp=$(sudo netstat -an | grep ":$port " | awk '$1 == "tcp" && $NF == "LISTEN" {print $0}' | wc -l)
+        udp=$(sudo netstat -an | grep ":$port " | awk '$1 == "udp" && $NF == "0.0.0.0:*" {print $0}' | wc -l)
+    done
+    #返回端口号
+    echo $port
 }
 
 #实现 TCP UDP端口监听互转的函数
 u2t_t2u(){
-  # 检查程序是否存在
-  if ! command -v udp2tcp &> /dev/null
-  then
-      echo "udp2tcp is not installed. Please install it first."
-      exit 1
-  fi
+    # 检查程序是否存在
+    if ! command -v udp2tcp &> /dev/null
+    then
+        echo "udp2tcp is not installed. Please install it first."
+        exit 1
+    fi
 
-  if ! command -v tcp2udp &> /dev/null
-  then
-      echo "tcp2udp is not installed. Please install it first."
-      exit 1
-  fi
+    if ! command -v tcp2udp &> /dev/null
+    then
+        echo "tcp2udp is not installed. Please install it first."
+        exit 1
+    fi
 
-  #指定UDP端口和TCP端口
-  UDP_PORT=$1
-  TCP_PORT=$2
+    #指定UDP端口和TCP端口
+    UDP_PORT=$1
+    TCP_PORT=$2
 
-  # 定义 tcp2udp 的 TCP 监听地址和 UDP 转发地址
-  TCP_LISTEN_ADDR="0.0.0.0:$TCP_PORT"
-  UDP_FORWARD_ADDR="127.0.0.1:$UDP_PORT"
+    # 定义 tcp2udp 的 TCP 监听地址和 UDP 转发地址
+    TCP_LISTEN_ADDR="0.0.0.0:$TCP_PORT"
+    UDP_FORWARD_ADDR="127.0.0.1:$UDP_PORT"
 
-  #创建一个后台进程，监听tcp的源端口，转发给udp的目标端口
-  nohup tcp2udp --tcp-listen $TCP_LISTEN_ADDR --udp-forward $UDP_FORWARD_ADDR > /dev/null 2>&1 & disown
+    #创建一个后台进程，监听tcp的源端口，转发给udp的目标端口
+    nohup tcp2udp --tcp-listen $TCP_LISTEN_ADDR --udp-forward $UDP_FORWARD_ADDR > /dev/null 2>&1 & disown
 
-  # 定义udp2tcp的UDP监听地址和TCP转发地址
-  UDP_LISTEN_ADDR="0.0.0.0:$UDP_PORT"
-  TCP_FORWARD_ADDR="127.0.0.1:$TCP_PORT"
+    # 定义udp2tcp的UDP监听地址和TCP转发地址
+    UDP_LISTEN_ADDR="0.0.0.0:$UDP_PORT"
+    TCP_FORWARD_ADDR="127.0.0.1:$TCP_PORT"
 
-  #创建一个后台进程，监听udp的目标端口，转发给tcp的源端口
-  nohup udp2tcp --udp-listen $UDP_LISTEN_ADDR --tcp-forward $TCP_FORWARD_ADDR > /dev/null 2>&1 & disown
+    #创建一个后台进程，监听udp的目标端口，转发给tcp的源端口
+    nohup udp2tcp --udp-listen $UDP_LISTEN_ADDR --tcp-forward $TCP_FORWARD_ADDR > /dev/null 2>&1 & disown
 
-  #显示后台进程的PID，方便结束时杀死
-  echo "TCP to UDP: $TCP_LISTEN_ADDR -> $UDP_FORWARD_ADDR"
-  echo "UDP to TCP: $UDP_FORWARD_ADDR -> $TCP_LISTEN_ADDR"
+    #显示后台进程的PID，方便结束时杀死
+    echo "TCP to UDP: $TCP_LISTEN_ADDR -> $UDP_FORWARD_ADDR"
+    echo "UDP to TCP: $UDP_FORWARD_ADDR -> $TCP_LISTEN_ADDR"
 }
 
 # 创建用户添加密码
 createUserNamePassword(){
-
     # 判断用户名
     if [[ -z "${USER_NAME}" ]]; then
-      echo "Please set 'USER_NAME' for linux"
-      exit 2
+        echo "Please set 'USER_NAME' for linux"
+        exit 2
     else
-      sudo useradd -m ${USER_NAME}
-      sudo adduser ${USER_NAME} sudo
+        sudo useradd -m ${USER_NAME}
+        sudo adduser ${USER_NAME} sudo
     fi
 
     # 判断设置用户密码环境变量
     if [[ -z "${USER_PW}" ]]; then
-      echo "Please set 'USER_PW' for linux"
-      exit 3
+        echo "Please set 'USER_PW' for linux"
+        exit 3
     else
-      echo "${USER_NAME}:${USER_PW}" | sudo chpasswd
-      sudo sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-      echo "Update linux user password !"
-      echo -e "${USER_PW}\n${USER_PW}" | sudo passwd "${USER_NAME}"
+        echo "${USER_NAME}:${USER_PW}" | sudo chpasswd
+        sudo sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+        echo "Update linux user password !"
+        echo -e "${USER_PW}\n${USER_PW}" | sudo passwd "${USER_NAME}"
     fi
 
     # 判断用户hostname
     if [[ -z "${HOST_NAME}" ]]; then
-      echo "Please set 'HOST_NAME' for linux"
-      exit 4
+        echo "Please set 'HOST_NAME' for linux"
+        exit 4
     else
-      sudo hostname ${HOST_NAME}
+        sudo hostname ${HOST_NAME}
     fi
 }
 
@@ -223,11 +222,11 @@ getStartSing-box_cloudflared_CloudflareSpeedTest_ngrok(){
 
     # 判断 Ngrok TOKEN 环境变量
     if [[ -z "${NGROK_AUTH_TOKEN}" ]]; then
-      echo "Please set 'NGROK_AUTH_TOKEN'"
-      exit 5
+        echo "Please set 'NGROK_AUTH_TOKEN'"
+        exit 5
     else
-      # Ngrok安装
-      curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+        # Ngrok安装
+        curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 
 # ngrok配置文件生成
 cat << EOL | sudo tee /home/${USER_NAME}/ngrok/ngrok.yml > /dev/null
@@ -250,62 +249,64 @@ tunnels:
     proto: tcp
     addr: ${U_FORWORD_T_PORT}
 EOL
-      # 应用 ngrok 配置
-      sudo ngrok config upgrade --config /home/${USER_NAME}/ngrok/ngrok.yml
-      # 启动 ngrok
-      sudo nohup ngrok start --all --config /home/${USER_NAME}/ngrok/ngrok.yml > /dev/null 2>&1 & disown
-      # 等待
-      sleep 10
-
+      
+    # 应用 ngrok 配置
+    sudo ngrok config upgrade --config /home/${USER_NAME}/ngrok/ngrok.yml
+    # 启动 ngrok
+    sudo nohup ngrok start --all --config /home/${USER_NAME}/ngrok/ngrok.yml > /dev/null 2>&1 & disown
+    # 等待
+    sleep 10
+    # 获取 ngrok 映射信息
+    NGROK_INFO=$(curl -s http://127.0.0.1:4040/api/tunnels)
     HAS_ERRORS=$(grep "command failed" < /home/${USER_NAME}/ngrok/ngrok.log)
 
     if [[ -z "$HAS_ERRORS" ]]; then
-      echo "=========================================="
-      # 获取 ngrok 映射信息
-      NGROK_INFO=$(curl -s http://127.0.0.1:4040/api/tunnels)
+        echo "=========================================="
+        # 获取 ngrok 映射信息
+        NGROK_INFO=$(curl -s http://127.0.0.1:4040/api/tunnels)
 
-      # 提取映射端口和域名
-      SSH_N_INFO=$(echo "$NGROK_INFO" | jq -r '.tunnels[] | select(.name=="ssh") | .public_url')
-      VLESS_N_INFO=$(echo "$NGROK_INFO" | jq -r '.tunnels[] | select(.name=="vless") | .public_url')
-      VMESS_N_INFO=$(echo "$NGROK_INFO" | jq -r '.tunnels[] | select(.name=="vmess") | .public_url')
-      SINGBOX_N_INFO=$(echo "$NGROK_INFO" | jq -r '.tunnels[] | select(.name=="sing-box") | .public_url')
+        # 提取映射端口和域名
+        SSH_N_INFO=$(echo "$NGROK_INFO" | jq -r '.tunnels[] | select(.name=="ssh") | .public_url')
+        VLESS_N_INFO=$(echo "$NGROK_INFO" | jq -r '.tunnels[] | select(.name=="vless") | .public_url')
+        VMESS_N_INFO=$(echo "$NGROK_INFO" | jq -r '.tunnels[] | select(.name=="vmess") | .public_url')
+        SINGBOX_N_INFO=$(echo "$NGROK_INFO" | jq -r '.tunnels[] | select(.name=="sing-box") | .public_url')
 
-      # 使用正则表达式提取域名和端口
-      SSH_N_DOMAIN=$(echo "$SSH_N_INFO" | awk -F[/:] '{print $4}')
-      SSH_N_PORT=$(echo "$SSH_N_INFO" | awk -F[/:] '{print $5}')
+        # 使用正则表达式提取域名和端口
+        SSH_N_DOMAIN=$(echo "$SSH_N_INFO" | awk -F[/:] '{print $4}')
+        SSH_N_PORT=$(echo "$SSH_N_INFO" | awk -F[/:] '{print $5}')
 
-      VLESS_N_DOMAIN=$(echo "$VLESS_N_INFO" | awk -F[/:] '{print $4}')
-      VLESS_N_PORT=$(echo "$VLESS_N_INFO" | awk -F[/:] '{print $5}')
+        VLESS_N_DOMAIN=$(echo "$VLESS_N_INFO" | awk -F[/:] '{print $4}')
+        VLESS_N_PORT=$(echo "$VLESS_N_INFO" | awk -F[/:] '{print $5}')
 
-      VMESS_N_DOMAIN=$(echo "$VMESS_N_INFO" | awk -F[/:] '{print $4}')
-      VMESS_N_PORT=$(echo "$VMESS_N_INFO" | awk -F[/:] '{print $5}')
+        VMESS_N_DOMAIN=$(echo "$VMESS_N_INFO" | awk -F[/:] '{print $4}')
+        VMESS_N_PORT=$(echo "$VMESS_N_INFO" | awk -F[/:] '{print $5}')
 
-      SINGBOX_N_DOMAIN=$(echo "$SINGBOX_N_INFO" | awk -F[/:] '{print $4}')
-      SINGBOX_N_PORT=$(echo "$SINGBOX_N_INFO" | awk -F[/:] '{print $5}')
+        SINGBOX_N_DOMAIN=$(echo "$SINGBOX_N_INFO" | awk -F[/:] '{print $4}')
+        SINGBOX_N_PORT=$(echo "$SINGBOX_N_INFO" | awk -F[/:] '{print $5}')
 
-      # 创建证书和密钥
-      sudo mkdir -pv /home/$USER_NAME/hysteria
-      sudo openssl ecparam -genkey -name prime256v1 -out /home/$USER_NAME/hysteria/private.key
-      sudo openssl req -new -x509 -days 36500 -key /home/$USER_NAME/hysteria/private.key -out /home/$USER_NAME/hysteria/cert.pem -subj "/CN="${SINGBOX_N_DOMAIN}
+        # 创建证书和密钥
+        sudo mkdir -pv /home/$USER_NAME/hysteria
+        sudo openssl ecparam -genkey -name prime256v1 -out /home/$USER_NAME/hysteria/private.key
+        sudo openssl req -new -x509 -days 36500 -key /home/$USER_NAME/hysteria/private.key -out /home/$USER_NAME/hysteria/cert.pem -subj "/CN="${SINGBOX_N_DOMAIN}
 
-      # 生成 reality 私钥公钥对
-      R_PRIVATEKEY_PUBLICKEY="$(sing-box generate reality-keypair)"
-      # 提取私钥
-      R_PRIVATEKEY="$(echo $R_PRIVATEKEY_PUBLICKEY | awk '{print $2}')"
-      # 提取公钥
-      R_PUBLICKEY="$(echo $R_PRIVATEKEY_PUBLICKEY | awk '{print $4}')"
-      
-      # 生成 uuid
-      V_UUID="$(sing-box generate uuid)"
-      VM_UUID="$(sing-box generate uuid)"
+        # 生成 reality 私钥公钥对
+        R_PRIVATEKEY_PUBLICKEY="$(sing-box generate reality-keypair)"
+        # 提取私钥
+        R_PRIVATEKEY="$(echo $R_PRIVATEKEY_PUBLICKEY | awk '{print $2}')"
+        # 提取公钥
+        R_PUBLICKEY="$(echo $R_PRIVATEKEY_PUBLICKEY | awk '{print $4}')"
 
-      # 生成16位16进制hex
-      R_HEX="$(sing-box generate rand --hex 8)"
-      VM_HEX="$(sing-box generate rand --hex 8)"
-      SB_HEX="$(sing-box generate rand --hex 8)"
+        # 生成 uuid
+        V_UUID="$(sing-box generate uuid)"
+        VM_UUID="$(sing-box generate uuid)"
 
-      # 生成12位26进制
-      WS_PATH="$(sing-box generate rand --hex 6)"
+        # 生成16位16进制hex
+        R_HEX="$(sing-box generate rand --hex 8)"
+        VM_HEX="$(sing-box generate rand --hex 8)"
+        SB_HEX="$(sing-box generate rand --hex 8)"
+
+        # 生成12位26进制
+        WS_PATH="$(sing-box generate rand --hex 6)"
 
 # 生成sing-box配置文件 写入配置文件
 cat << EOL | sudo tee /etc/sing-box/config.json > /dev/null
@@ -785,23 +786,27 @@ EOL
       UDP2TCP_INFO=$(u2t_t2u ${SB_PORT} ${U_FORWORD_T_PORT})
 cat << EOL | sudo tee result.txt > /dev/null
 SSH is accessible at: 
-                      ${HOSTNAME_IP}:22 -> ${SSH_N_DOMAIN}:${SSH_N_PORT}
-                      ssh -p ${SSH_N_PORT} -o ServerAliveInterval=60 ${USER_NAME}@${SSH_N_DOMAIN}
+${HOSTNAME_IP}:22 -> ${SSH_N_DOMAIN}:${SSH_N_PORT}
+ssh -p ${SSH_N_PORT} -o ServerAliveInterval=60 ${USER_NAME}@${SSH_N_DOMAIN}
+
 VLESS is accessible at: 
-                       ${HOSTNAME_IP}:${V_PORT} -> ${VLESS_N_DOMAIN}:${VLESS_N_PORT}
+${HOSTNAME_IP}:${V_PORT} -> ${VLESS_N_DOMAIN}:${VLESS_N_PORT}
+
 VMESS is accessible at: 
-                       ${HOSTNAME_IP}:${VM_PORT} -> ${VMESS_N_DOMAIN}:${VMESS_N_PORT}
+${HOSTNAME_IP}:${VM_PORT} -> ${VMESS_N_DOMAIN}:${VMESS_N_PORT}
+
 Sing-Box is accessible at: 
-                           ${HOSTNAME_IP}:${SB_PORT} -> ${SINGBOX_N_DOMAIN}:${SINGBOX_N_PORT}
+${HOSTNAME_IP}:${SB_PORT} -> ${SINGBOX_N_DOMAIN}:${SINGBOX_N_PORT}
+
 Time Frame is accessible at: 
-                            ${REPORT_DATE}~${F_DATE}
+${REPORT_DATE}~${F_DATE}
 ${UDP2TCP_INFO}
 EOL
 
-      echo "=========================================="
+        echo "=========================================="
     else
-      echo "$HAS_ERRORS"
-      exit 6
+        echo "$HAS_ERRORS"
+        exit 6
     fi
 }
 
